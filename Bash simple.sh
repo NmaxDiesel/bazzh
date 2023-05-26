17f357885ccf692e8fee190d6365afd80eb46536
NmaxDiesel <123163060+NmaxDiesel@users.noreply.github.com>
2023-05-26 20:30:21 +0700

    Add files via upload

---
+++ Bash simple.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Selamat datang di program sederhana!"
+echo "------------------------------"
+
+read -p "Silakan masukkan nama Anda: " nama
+
+echo "Halo, $nama! Selamat datang di program ini."
+echo "Terima kasih telah menggunakan program kami."
